655d80831e9a7d7823a3f1a300160ae60502ec5b
Jason <jason.wang@sun-innovation.com>
2017-05-08 12:18:51 +0800

    修正supported_classes取得view/sync中兩層class問題

--- lib/render_sync/refetch_model.rb
+++ lib/render_sync/refetch_model.rb
@@ -1,7 +1,7 @@
 module RenderSync
   class RefetchModel
 
-    def self.find_by_class_name_and_id(resource_name, id)      
+    def self.find_by_class_name_and_id(resource_name, id)
       class_name = resource_name.to_s.classify
       class_name.safe_constantize.find(id) if supported_classes.include?(class_name)
     rescue
@@ -10,10 +10,10 @@ module RenderSync
 
     def self.supported_classes
       Thread.current["sync_refetch_classes"] = nil if Rails.env.development?
-      
+
       Thread.current["sync_refetch_classes"] ||= begin
         Dir["app/views/sync/*/refetch"].collect{|path|
-          File.basename(path.gsub(/\/refetch$/, '')).classify
+          path.split(/sync\/(.*?)\/refetch/)[1].classify
         }.reject{|clazz| clazz.nil? }
       end
     end
